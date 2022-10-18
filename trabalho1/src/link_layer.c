0c9304e37fbf6dab48eb38e24f551bb577326ffd
Bruno Pascoal <brunompp98@gmail.com>
2022-10-18 19:21:04 +0100

    Organize link_layer file

--- trabalho1/src/link_layer.c
+++ trabalho1/src/link_layer.c
@@ -47,142 +47,16 @@ typedef struct
     unsigned char bcc;
     unsigned char *data;
     unsigned int data_size;
-} State;
+} Trama;
 
 int fd;
-State state;
+Trama trama;
 struct termios oldtio;
 struct termios newtio;
 LinkLayer linker;
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
-
-void state_machine_handler(State *state, unsigned char byte)
-{
-    switch (state->state)
-    {
-    case S_START:
-        if (byte == FLAG)
-            state->state = S_FLAG;
-        break;
-    case S_FLAG:
-        if (byte == A_SEND || byte == A_RESPONSE)
-        {
-            state->state = S_ADR;
-            state->adr = byte;
-            break;
-        }
-        state->state = S_START;
-        break;
-    case S_ADR:
-        if (byte == C_SET || byte == C_DISC || byte == C_UA || byte == C_RR_0 || byte == C_RR_1 || byte == C_RJ_0 || byte == C_RJ_1)
-        {
-            state->state = S_CTRL;
-            state->ctrl = byte;
-            state->bcc = createBCC_header(state->adr, state->ctrl);
-            break;
-        }
-        state->state = S_START;
-        break;
-    case S_CTRL:
-    case S_BCC1:
-    case S_BCC2:
-    case S_DATA:
-    case S_ESC:
-    case S_END:
-    case S_REJ:
-    }
-}
-
-////////////////////////////////////////////////
-// LLOPEN
-////////////////////////////////////////////////
-int llopen(LinkLayer connectionParameters)
-{
-    // TODO
-    linker = connectionParameters;
-
-    int fd = open(linker.serialPort, O_RDWR | O_NOCTTY);
-
-    if (fd < 0)
-    {
-        perror(linker.serialPort);
-        exit(-1);
-    }
-    // Save current port settings
-    if (tcgetattr(fd, &oldtio) == -1)
-    {
-        perror("tcgetattr");
-        exit(-1);
-    }
-
-    // Clear struct for new port settings
-    memset(&newtio, 0, sizeof(newtio));
-
-    newtio.c_cflag = linker.baudRate | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    // Set input mode (non-canonical, no echo,...)
-    newtio.c_lflag = 0;
-    newtio.c_cc[VTIME] = 30; // Inter-character timer unused
-    newtio.c_cc[VMIN] = 0;   // Blocking read until 5 chars received
-
-    // VTIME e VMIN should be changed in order to protect with a
-    // timeout the reception of the following character(s)
-
-    // Now clean the line and activate the settings for the port
-    // tcflush() discards data written to the object referred to
-    // by fd but not transmitted, or data received but not read,
-    // depending on the value of queue_selector:
-    //   TCIFLUSH - flushes data received but not read.
-    tcflush(fd, TCIOFLUSH);
-
-    // Set new port settings
-    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-    {
-        perror("tcsetattr");
-        exit(-1);
-    }
-
-    return 1;
-}
-
-////////////////////////////////////////////////
-// LLWRITE
-////////////////////////////////////////////////
-int llwrite(const unsigned char *buf, int bufSize)
-{
-    // TODO
-
-    return 0;
-}
-
-////////////////////////////////////////////////
-// LLREAD
-////////////////////////////////////////////////
-int llread(unsigned char *packet)
-{
-    // TODO
-
-    return 0;
-}
-
-////////////////////////////////////////////////
-// LLCLOSE
-////////////////////////////////////////////////
-int llclose(int showStatistics)
-{
-    // Restore the old port settings
-    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
-    {
-        perror("tcsetattr");
-        exit(-1);
-    }
-
-    return 1;
-}
 
 ///////////////////////////////////////////
 // HELPER FUNCTIONS
@@ -353,4 +227,138 @@ int byteDestuffing(unsigned char *frame, int length)
     }
 
     return header_jump;
+}
+
+void state_machine_handler(Trama *trama, unsigned char byte)
+{
+    switch (trama->state)
+    {
+    case S_START:
+        if (byte == FLAG)
+            trama->state = S_FLAG;
+        break;
+    case S_FLAG:
+        if (byte == A_SEND || byte == A_RESPONSE)
+        {
+            trama->state = S_ADR;
+            trama->adr = byte;
+            break;
+        }
+        trama->state = S_START;
+        break;
+    case S_ADR:
+        if (byte == C_SET || byte == C_DISC || byte == C_UA || byte == C_RR_0 || byte == C_RR_1 || byte == C_RJ_0 || byte == C_RJ_1)
+        {
+            trama->state = S_CTRL;
+            trama->ctrl = byte;
+            trama->bcc = createBCC_header(trama->adr, trama->ctrl);
+            break;
+        }
+        trama->state = S_START;
+        break;
+    case S_CTRL:
+        if (byte == trama->bcc)
+        {
+            trama->state = S_BCC1;
+            break;
+        }
+        trama->state = S_START;
+        break;
+    case S_BCC1:
+    case S_BCC2:
+    case S_DATA:
+    case S_ESC:
+    case S_END:
+        trama->state = S_START;
+    case S_REJ:
+    }
+}
+
+////////////////////////////////////////////////
+// LLOPEN
+////////////////////////////////////////////////
+int llopen(LinkLayer connectionParameters)
+{
+    // TODO
+    linker = connectionParameters;
+
+    int fd = open(linker.serialPort, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)
+    {
+        perror(linker.serialPort);
+        exit(-1);
+    }
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = linker.baudRate | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 30; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 0;   // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    return 1;
+}
+
+////////////////////////////////////////////////
+// LLWRITE
+////////////////////////////////////////////////
+int llwrite(const unsigned char *buf, int bufSize)
+{
+    // TODO
+
+    return 0;
+}
+
+////////////////////////////////////////////////
+// LLREAD
+////////////////////////////////////////////////
+int llread(unsigned char *packet)
+{
+    // TODO
+
+    return 0;
+}
+
+////////////////////////////////////////////////
+// LLCLOSE
+////////////////////////////////////////////////
+int llclose(int showStatistics)
+{
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    return 1;
 }
